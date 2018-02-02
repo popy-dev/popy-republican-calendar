bdab900c2eeb49caac113aea4a96d30fdcdffd8b
Pierre Dudoret <pierre.dudoret@potogan.fr>
2018-02-02 21:48:55 +0100

    [Converter] Factorized converter code

--- src/Converter/RelativeTimestampLeapYear.php
+++ src/Converter/RelativeTimestampLeapYear.php
@@ -9,12 +9,14 @@ use Popy\RepublicanCalendar\ConverterInterface;
 use Popy\Calendar\Converter\TimeConverterInterface;
 use Popy\Calendar\Converter\TimeConverter\DuoDecimalTime;
 use Popy\Calendar\Converter\LeapYearCalculatorInterface;
+use Popy\Calendar\Converter\AbstractPivotalDateSolarYear;
 use Popy\RepublicanCalendar\Converter\LeapYearCalculator\RommeWithContinuedImpairLeapDay;
+
 
 /**
  * Finest converter implementation i could make.
  */
-class RelativeTimestampLeapYear implements ConverterInterface
+class RelativeTimestampLeapYear extends AbstractPivotalDateSolarYear implements ConverterInterface
 {
     /**
      * Year 1 date (will result in a year 0 index, incremented to be displayed).
@@ -22,18 +24,6 @@ class RelativeTimestampLeapYear implements ConverterInterface
      * 1792-09-22 00:00:00
      */
     const REVOLUTION_ERA_START = -5594227200;
-
-    /**
-     * Self-explanatory.
-     */
-    const SECONDS_PER_DAY = 24 * 3600;
-
-    /**
-     * Leap year calculator.
-     *
-     * @var LeapYearCalculatorInterface
-     */
-    protected $calculator;
 
     /**
      * Time converter.
@@ -50,53 +40,30 @@ class RelativeTimestampLeapYear implements ConverterInterface
      */
     public function __construct(LeapYearCalculatorInterface $calculator = null, TimeConverterInterface $timeConverter = null)
     {
-        $this->calculator = $calculator ?: new RommeWithContinuedImpairLeapDay();
+        parent::__construct($calculator ?: new RommeWithContinuedImpairLeapDay());
         $this->timeConverter = $timeConverter ?: new DuoDecimalTime();
     }
 
     /**
-     * {@inheritDoc}
+     * @inheritDoc
+     */
+    protected function getEraStart()
+    {
+        return static::REVOLUTION_ERA_START;
+    }
+
+    /**
+     * @inheritDoc
      */
     public function toRepublican(DateTimeInterface $input)
     {
-        $offset = intval($input->format('Z'));
-        // Use a timestamp relative to the first revolutionnary year and
-        // including timezone offset
-        $relativeTimestamp = $input->getTimestamp()
-            - static::REVOLUTION_ERA_START
-            + $offset
-        ;
-
-        $eraDayIndex = intval($relativeTimestamp / static::SECONDS_PER_DAY);
-        $year = 1;
-
-        // Will exit once the negative year will be found
-        while ($eraDayIndex < 0) {
-            $dayCount = 365 + $this->calculator->isLeapYear($year - 1);
-
-            $eraDayIndex += $dayCount;
-            $year--;
-        }
-
-        while (true) {
-            $dayCount = 365 + $this->calculator->isLeapYear($year);
-
-            if ($eraDayIndex < $dayCount) {
-                // $year and dayIndex found !
-                break;
-            }
-
-            $eraDayIndex -= $dayCount;
-            $year++;
-        }
-
-        $remainingMicroSeconds = intval($input->format('u'))
-            + ($relativeTimestamp % static::SECONDS_PER_DAY) * 1000000
+        list($year, $dayIndex, $microsec, $offset) = $this->
+            fromDateTimeInterface($input)
         ;
 
         $res = new RepublicanDateTime(
             $year,
-            $eraDayIndex,
+            $dayIndex,
             $this->calculator->isLeapYear($year),
             $input->getTimezone(),
             $offset
@@ -105,7 +72,7 @@ class RelativeTimestampLeapYear implements ConverterInterface
         return $res
             ->setTimestamp($input->getTimestamp())
             ->setTime(
-                $this->timeConverter->fromMicroSeconds($remainingMicroSeconds)
+                $this->timeConverter->fromMicroSeconds($microsec)
             )
         ;
     }
@@ -115,59 +82,15 @@ class RelativeTimestampLeapYear implements ConverterInterface
      */
     public function fromRepublican(RepublicanDateTime $input)
     {
-        $dayIndex = $input->getDayIndex();
-        $year = $input->getYear();
-
-        $sign = $year < 1 ? -1 : 1;
-
-        for ($i=min($year, 1); $i < max($year, 1); $i++) {
-            $dayCount = 365 + $this->calculator->isLeapYear($i);
-            $dayIndex += $sign * $dayCount;
-        }
-
-        // todo timezone handling !
-        $timestamp = static::REVOLUTION_ERA_START
-            + ($dayIndex * static::SECONDS_PER_DAY)
-        ;
-
-        $remainingMicroSeconds = $this->timeConverter->toMicroSeconds(
+        $microsec = $this->timeConverter->toMicroSeconds(
             $input->getTime()
         );
 
-        $timestamp += intval($remainingMicroSeconds) / 1000000;
-        $microseconds = $remainingMicroSeconds % 1000000;
-
-        // Looking for timezone offset matching the incomplete timestamp.
-        // The LMT transition is skipped to mirror the behaviour of
-        // DateTimeZone->getOffset()
-        $offset = 0;
-        $previous = null;
-        $offsets = $input->getTimezone()->getTransitions(
-            $timestamp-static::SECONDS_PER_DAY
+        return $this->toDateTimeInterface(
+            $input->getYear(),
+            $input->getDayIndex(),
+            $microsec,
+            $input->getTimezone()
         );
-        foreach ($offsets as $info) {
-            if (
-                (!$previous || $previous['abbr'] !== 'LMT')
-                && $timestamp - $info['offset'] < $info['ts']
-            ) {
-                break;
-            }
-
-            $previous = $info;
-
-            $offset = $info['offset'];
-        }
-
-        $timestamp -= $offset;
-
-        $timestring = sprintf(
-            '%s.%06d UTC',
-            $timestamp,
-            $microseconds
-        );
-
-        return DateTimeImmutable::createFromFormat('U.u e', $timestring)
-            ->setTimezone($input->getTimezone())
-        ;
     }
 }
